5166fe9154c5d8b07c9ebec2bdb854435f966c00
kiki8388 <70375140+kiki8388@users.noreply.github.com>
2021-06-20 22:36:35 +0200

    bug fixes

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3001/api' || 'https://mynotespwi.herokuapp.com/'
+  baseURL: process.env.API_URL || 'http://localhost:3001/api' || 'https://mynotespwi.herokuapp.com/api/notes'
 });
 
 export default instance;
